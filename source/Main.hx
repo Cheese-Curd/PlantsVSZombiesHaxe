e3562f22e84a9909d9433c78697d3c85b8aa0a29
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2021-09-03 17:59:25 -0400

    Added a mouse

--- source/Main.hx
+++ source/Main.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.FlxSprite;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -9,7 +10,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, MainMenuState));
+		addChild(new FlxGame(0, 0, LoadingState));
 		FlxG.autoPause = false;
+
+		// mouse \\
+		FlxG.mouse.load('assets/images/cursor.png');
 	}
 }
