44b5d97dcc33e62303da72d53a2ab02d64b39637
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2021-09-03 17:59:45 -0400

    Removed the old mouse code and added sunflower

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -5,6 +5,7 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.system.FlxSound;
 import flixel.tweens.FlxTween;
+import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 
 using flixel.util.FlxSpriteUtil;
@@ -12,6 +13,7 @@ using flixel.util.FlxSpriteUtil;
 class MainMenuState extends FlxState
 {
 	var peashooter:FlxSprite;
+	var sunflower:FlxSprite;
 	var frame = 0;
 	var framerate = 30;
 
@@ -19,29 +21,39 @@ class MainMenuState extends FlxState
 	{
 		super.create();
 
-		var cursor:FlxSprite;
-
 		// Plays the Main Menu Theme (Dave Intro) \\
 		FlxG.sound.playMusic('assets/music/main_menu_theme.ogg');
 
 		peashooter = new FlxSprite();
+		sunflower = new FlxSprite();
 
-		// mouse \\
-		cursor = new FlxSprite();
-		cursor.loadGraphic('assets/images/cursor.png');
-		FlxG.mouse.load(cursor);
-
-		peashooter.loadGraphic('assets/images/peashooter.png', true, 250, 250);
+		peashooter.loadGraphic('assets/images/plants/peashooter.png', true, 254, 254);
+		sunflower.loadGraphic('assets/images/plants/sunflower.png', true, 189, 189);
 
 		// Animation \\
 		peashooter.animation.add("idle", framesArray(49), 30, true);
+		sunflower.animation.add("idle", framesArray(54), 30, true);
+		// Antialiasing \\
+		peashooter.antialiasing = true;
+		sunflower.antialiasing = true;
 
 		add(peashooter);
+		add(sunflower);
 		peashooter.x = 0;
 		peashooter.y = 0;
+		sunflower.x = 100;
+		sunflower.y = 0;
+		sunflower.animation.play("idle");
+		peashooter.animation.play("idle");
+		var playbutton = new FlxButton(0, 0, 'MainMenuState', function()
+		{
+			FlxG.switchState(new PlayState());
+			FlxG.sound.music.stop();
+		});
+		add(playbutton);
 	}
 
-	// stuff for animation that might be scrapped lol \\
+	// Animation Frames shit so it doesn't make the file giant \\
 	function framesArray(num:Int)
 	{
 		var array:Array<Int> = new Array<Int>();
