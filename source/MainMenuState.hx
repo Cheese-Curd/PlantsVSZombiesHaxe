74f13fe3706e58c983268cfaff2630a75af3adea
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2021-09-02 21:09:32 -0400

    haha, cursor go BRRRRRRRRRRRR

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.system.FlxSound;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
@@ -15,18 +16,38 @@ class MainMenuState extends FlxState
 		super.create();
 
 		var sprite_test:FlxSprite;
+		var cursor:FlxSprite;
 
 		// Plays the Main Menu Theme (Dave Intro)
 		FlxG.sound.playMusic('assets/music/main_menu_theme.ogg', 1, true);
 
 		sprite_test = new FlxSprite();
+		cursor = new FlxSprite();
+
 		sprite_test.loadGraphic('assets/images/peashooter_placeholder.png');
 		sprite_test.x = 100;
 		sprite_test.y = 0;
 
+		cursor.loadGraphic('icons/cursor.png');
+
+		FlxG.mouse.load(cursor);
+
 		add(sprite_test);
 
 		FlxTween.tween(sprite_test, {x: FlxG.width - sprite_test.width, y: FlxG.height - sprite_test.height, angle: 360.0}, 5, {type: FlxTweenType.PINGPONG});
+	}
+
+	// HAXE NOOOOOOOOOOOOOOOOOOOOOOOOO
+	override public function onFocusLost()
+	{
+		super.onFocusLost();
+		FlxG.sound.music.pause();
+	}
+
+	override public function onFocus()
+	{
+		super.onFocus();
+		FlxG.sound.music.resume();
 	}
 
 	override public function update(elapsed:Float)
