b642f2940bb8db6988094cc74bfdf5a3d0694432
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2021-09-05 11:56:54 -0400

    why the hell is my game not responding

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -1,25 +1,93 @@
 package;
 
+import AngelUtils; // for masking lol
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.input.actions.FlxAction;
+import flixel.system.FlxAssets;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
+import flixel.util.FlxSpriteUtil;
 import flixel.util.FlxTimer;
 
 class LoadingState extends FlxState
 {
+	var popcap_logo:FlxSprite;
+	var popcap:FlxText;
+	var cheese:FlxText;
+	// PvZ Logo \\
+	var pvz_logo:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
-		var fuckingbutton = new FlxButton(0, 0, 'Click me to Start!', function()
-		{
-			FlxG.switchState(new MainMenuState());
-		});
-		fuckingbutton.scale.set(1.5);
+		FlxG.sound.playMusic('assets/music/main_menu_theme.ogg');
+		FlxAssets.FONT_DEFAULT = 'assets/fonts/Brianne_s_hand.ttf';
+		popcap = new FlxText(231, 38, 339, 'Plants VS Zombies made by:', 24);
+		cheese = new FlxText(184, 377, 433, 'Haxe Edition made by: Cheese Curd', 24);
+
+		popcap_logo = new FlxSprite(250, 65).loadGraphic('assets/images/menu/loading/PopCap_Logo.jpg');
+
+		add(popcap);
+		add(cheese);
+		add(popcap_logo);
+		fadeAnimation();
+
+		// var fuckingbutton = new FlxButton(0, 0, 'Click me to Start!', function()
+		// {
+		// 	FlxG.switchState(new MainMenuState());
+		// });
+		// fuckingbutton.scale.set(1.5);
 		// HAXE NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO #2
-		new FlxTimer().start(5, function(tmr:FlxTimer)
+		// new FlxTimer().start(5, function(tmr:FlxTimer)
+		// {
+		// 	add(fuckingbutton);
+		// });
+	}
+
+	function fadeAnimation()
+	{
+		var alpha = 0.0;
+		while (alpha != 1)
 		{
-			add(fuckingbutton);
+			popcap_logo.alpha = alpha;
+			popcap.alpha = alpha;
+			alpha += 0.1;
+		};
+		new FlxTimer().start(2.0, function(tmr:FlxTimer)
+		{
+			textPlay();
 		});
+	};
+
+	function textPlay()
+	{
+		var alpha = 0.0;
+		while (alpha != 1)
+		{
+			cheese.alpha = alpha;
+			alpha += 0.1;
+		};
+		new FlxTimer().start(5.0, function(tmr:FlxTimer)
+		{
+			mainLoading();
+		});
+	};
+
+	function mainLoading()
+	{
+		// remove everything not needed \\
+		remove(cheese);
+		remove(popcap_logo);
+		remove(popcap);
+		// add background \\
+		var background:FlxSprite;
+		background = new FlxSprite().loadGraphic('assets/images/menu/loading/titlescreen.jpg');
+		// PvZ Logo \\
+		pvz_logo = AngelUtils.fromAlphaMask('assets/images/menu/loading/PvZ_Logo.jpg', 'assets/images/menu/loading/PvZ_Logo_.png');
+		add(pvz_logo);
 	}
 
 	override public function update(elapsed:Float)
